d6fbb2b4d82625bd74e86c777d62fe8a2d0b6f2e
GenPurCM <genpurcm@gmail.com>
2018-11-27 15:27:07 -0300

    A lot of progress!

---
+++ demo/src/main/resources/static/js/API-Requests.js
@@ -0,0 +1,38 @@
+function GetPlayerList() {
+    $.ajax({
+        type: 'GET',
+//        contentType: "application/json",
+//        dataType: "json",
+        url: "/players",
+//        processData: false,
+//        contentType: false,
+        success: function(data){
+            $.each(data, function(idx, item){
+                var birthdayItem = new Date(item.birthday);
+                var birthday = (birthdayItem.getMonth() + 1) + "/" + birthdayItem.getDate() + "/" + birthdayItem.getFullYear();
+                $("#table_body").append("<tr><td>" + idx + "</td><td>" + item.first_Name + "</td><td>" + item.last_Name + "</td><td>" + item.email_Address + "</td><td>" + birthday + "</td></tr>")
+            });
+            $('#added_items').show("slow");
+        },
+    });
+};
+
+$('#Ajax2').on("click", function(){
+    $.ajax({
+        type: 'GET',
+//        contentType: "application/json",
+//        dataType: "json",
+        url: "/players",
+        async:true,
+     }).done(function(data, textStatus, jqXHR){
+            if (textStatus == "success"){
+                console.log("Ajax2 Button Success");
+            } else {
+                alert("Ajax2 Button No Success");
+            };
+        }).fail(function( jqXHR, textStatus, errorThrown){
+             console.log("The error thrown is: " + errorThrown);
+        });
+});
+
+
