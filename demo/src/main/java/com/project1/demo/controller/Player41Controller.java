9add3712a171fb544b10b9b589758de902dd1c23
GenPurCM <genpurcm@gmail.com>
2019-01-09 19:46:47 -0300

    Seccion 41 - AJAX call to update Thymeleaf fragment! MORE

--- demo/src/main/java/com/project1/demo/controller/Player41Controller.java
+++ demo/src/main/java/com/project1/demo/controller/Player41Controller.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+//@RestController
+@Controller
 public class Player41Controller {
 
     @Autowired
@@ -30,12 +33,27 @@ public class Player41Controller {
 //        return "/index";
 //    }
 
-
     @GetMapping("/players411")
     public Page<Player> showCrud(Model model, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
 //        model.addAttribute("data", );
 //        model.addAttribute("currentPage", page);
 //        model.addAttribute("currentSize", size);
         return player2Repository.findAll(PageRequest.of(page, size));
+    }
+
+    @GetMapping("/players4111")
+    public String showCrud4111(Model model, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
+        model.addAttribute("data", player2Repository.findAll(PageRequest.of(page, size)));
+        model.addAttribute("currentPage", page);
+        model.addAttribute("currentSize", size);
+        return "test :: resultsList";
+    }
+
+    @GetMapping("/test")
+    public String showCrudtest(ModelMap model, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
+        model.addAttribute("data", player2Repository.findAll(PageRequest.of(page, size)));
+        model.addAttribute("currentPage", page);
+        model.addAttribute("currentSize", size);
+        return "test :: resultsList";
     }
 }
